33c2ceb6a30217b97be94aa8522679c86e44e380
Osmar Valero <osmarvalero100@gmail.com>
2026-08-17 17:04:37 -0500

    feat: add login

--- src/components/ui/submit-button.tsx
+++ src/components/ui/submit-button.tsx
@@ -1,10 +1,11 @@
 'use client'
 
+import React from 'react'
 import { useFormStatus } from 'react-dom'
-import { Button, ButtonProps } from '@/components/ui/button'
+import { Button } from '@/components/ui/button'
 import { Loader2 } from 'lucide-react'
 
-interface SubmitButtonProps extends ButtonProps {
+export interface SubmitButtonProps extends React.ComponentProps<typeof Button> {
   loadingText?: string
 }
 
